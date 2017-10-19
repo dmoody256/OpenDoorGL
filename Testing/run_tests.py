73c459b82f70fd28107fdc6e7ee9a2a4a138c25b
Daniel Moody <dmoody256@gmail.com>
2017-10-19 01:43:31 -0400

    print output on fail

--- Testing/run_tests.py
+++ Testing/run_tests.py
@@ -3,9 +3,11 @@ import subprocess
 import platform
 
 SikuliPath = os.environ["SIKULI_DIR"]
+try:
+    output = subprocess.check_output([SikuliPath + "/runsikulix", "-r", "cubeTestRed.sikuli"])
+    print("test result: " + str(output))
 
-output = subprocess.check_output([SikuliPath + "/runsikulix", "-r", "cubeTestRed.sikuli"])
-print("test result: " + str(output))
-
-output = subprocess.check_output([SikuliPath + "/runsikulix", "-r", "StackedCubeTest.sikuli"])
-print("test result: " + str(output))
+    output = subprocess.check_output([SikuliPath + "/runsikulix", "-r", "StackedCubeTest.sikuli"])
+    print("test result: " + str(output))
+except:
+    print output 
