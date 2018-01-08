b8111399041076500ac1b7cdb2df4f171b88b74e
Daniel Moody <dmoody256@gmail.com>
2018-01-08 02:37:02 -0500

    added script to setup Sikiuli for testing.

---
+++ Testing/install_sikiluX.sh
@@ -0,0 +1,5 @@
+export SIKULI_DIR=$PWD/SikuliX
+mkdir SikuliX && cd SikuliX
+wget https://launchpad.net/sikuli/sikulix/1.1.1/+download/sikulixsetup-1.1.1.jar
+java -jar sikulixsetup-1.1.1.jar options 1.1
+cd ..
