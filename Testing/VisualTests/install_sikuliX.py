7b3de3e0dd94e214e50e84ea65c6e282e1528b96
Daniel <dmoody256@gmail.com>
2018-07-09 01:04:18 -0400

    updated sikuli version

--- Testing/VisualTests/install_sikuliX.py
+++ Testing/VisualTests/install_sikuliX.py
@@ -11,14 +11,14 @@ import subprocess
 if not os.path.exists("SikuliX"):
     os.makedirs("SikuliX")
 
-url = 'https://launchpad.net/sikuli/sikulix/1.1.1/+download/sikulixsetup-1.1.1.jar'
+url = 'https://launchpad.net/sikuli/sikulix/1.1.2/+download/sikulixsetup-1.1.2.jar'
 response = pyurl.urlopen(url)
 
-with open("SikuliX/sikulixsetup-1.1.1.jar", 'wb') as f:
+with open("SikuliX/sikulixsetup-1.1.2.jar", 'wb') as f:
     jarFile = bytearray(response.read())
     f.write(jarFile)
 
-install_sikuli = ["java", "-jar", "sikulixsetup-1.1.1.jar", "options", "1.1"]
+install_sikuli = ["java", "-jar", "sikulixsetup-1.1.2.jar", "options", "1.1"]
 
 proc = subprocess.Popen(
     install_sikuli,
