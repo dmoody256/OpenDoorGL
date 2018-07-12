08fcdbb3ae50844488591f1028b937f5b4e9893c
Daniel <dmoody256@gmail.com>
2018-07-11 20:47:30 -0400

    removed old scripts

--- Testing/VisualTests/install_sikuliX.sh
+++
@@ -1,6 +0,0 @@
-export SIKULI_DIR=$PWD/SikuliX
-export DISPLAY=:0
-mkdir SikuliX && cd SikuliX
-wget https://launchpad.net/sikuli/sikulix/1.1.1/+download/sikulixsetup-1.1.1.jar
-java -jar sikulixsetup-1.1.1.jar options 1.1
-cd ..
