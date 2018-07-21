42acefe965a018fe0157bb01574016b375e755a8
Daniel Moody <dmoody256@gmail.com>
2018-07-20 22:44:06 -0500

    added typing to group storage

--- Core/Source/odgl_Group.cpp
+++ Core/Source/odgl_Group.cpp
@@ -13,25 +13,42 @@ namespace OpenDoorGL{
     Group::Group(){
         
     }
+
     Group::~Group(){
         for(int i = 0; i < objects.size(); i++){
-            delete objects.at(i);
+            switch(objects.at(i).first)
+            {
+                case ObjectType::RenderObjectType:
+                case ObjectType::UnknownType:
+                    delete objects.at(i).second;
+                    break;
+                case ObjectType::GeometricObjectType:
+                    delete (GeometricObject*)objects.at(i).second;
+                    break;
+                default:
+                    delete objects.at(i).second;
+                    break;
+            }
         }
     }
 
     void Group::draw(View* view){
         for(int i = 0; i < objects.size(); i++){
-            objects.at(i)->draw(view);
+            objects.at(i).second->draw(view);
         }
     }
     
     void Group::InsertObject(RenderObject* object){
-        objects.push_back(object);
+        objects.push_back(std::make_pair( ObjectType::RenderObjectType, object));
+    }
+
+    void Group::InsertObject(GeometricObject* object){
+        objects.push_back(std::make_pair( ObjectType::GeometricObjectType, (RenderObject*)object));
     }
 
     void Group::Update(double time_passed){
         for(int i = 0; i < objects.size(); i++){
-            objects.at(i)->Update(time_passed);
+            objects.at(i).second->Update(time_passed);
         }
     }
 
--- Core/Source/odgl_Group.hpp
+++ Core/Source/odgl_Group.hpp
@@ -8,22 +8,31 @@
 
 #include "odgl_Include.hpp"
 #include "odgl_RenderObject.hpp"
+#include "odgl_GeometricObject.hpp"
 
 namespace OpenDoorGL
 {
 
 class ODGL_DLLEXPORT Group : public RenderObject
 {
-
+  
   public:
     Group();
     ~Group();
     virtual void draw(View *view);
     virtual void InsertObject(RenderObject *object);
+    virtual void InsertObject(GeometricObject *object);
     virtual void Update(double time_passed);
 
   protected:
-    std::vector<RenderObject *> objects;
+
+    enum ObjectType{
+      RenderObjectType,
+      GeometricObjectType,
+      UnknownType
+    };
+
+    std::vector<std::pair<ObjectType, RenderObject*> > objects;
 };
 } // namespace OpenDoorGL
 
