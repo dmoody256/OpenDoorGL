5ccb542c3d9eb1b9a05f45a6da4afb850834b352
Daniel Moody <dmoody256@gmail.com>
2018-07-21 13:59:02 -0500

    updated to build button test

--- Core/Source/odgl_Include.cpp
+++ Core/Source/odgl_Include.cpp
@@ -4,15 +4,16 @@
 #include <iostream>
 #include <iomanip>
 
-namespace OpenDoorGL{
+namespace OpenDoorGL
+{
 
-    void CheckOpenGLError(const char* stmt, const char* fname, int line)
+void CheckOpenGLError(const char *stmt, const char *fname, int line)
+{
+    GLenum err = glGetError();
+    if (err != GL_NO_ERROR)
     {
-        GLenum err = glGetError();
-        if (err != GL_NO_ERROR)
-        {
-            std::cout << "OpenGL error 0x" << std::setfill('0') << std::setw(8) << std::hex << err << ", at " << fname << ":" << line << " - for " << stmt << std::endl;
-            abort();
-        }
+        std::cout << "OpenGL error 0x" << std::setfill('0') << std::setw(8) << std::hex << err << std::dec << ": " << gluErrorString(err) << ", at " << fname << ":" << line << " - for " << stmt << std::endl;
+        abort();
     }
 }
+} // namespace OpenDoorGL
